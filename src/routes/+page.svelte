2608d4871ae5c28f63778106830bd96f04aecc4c
Anas <anascs2023@gmail.com>
2024-06-25 07:14:20 +0300

    AM OR PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,24 @@
+<script lang="ts">
+    import dayjs from 'dayjs';
+    import 'dayjs/locale/ar';
+    
+    dayjs.locale('ar');
+    
+    let period= dayjs().format('a') =='م'?'مساء': 'صباح';
+</script>
+
 <div class="p-12">
     <header class="flex justify-between">
         <div>
-            <h1 class ="text-4xl mb-3  ">
-                صباح الخير 👋🏻
+            <h1 class ="text-4xl mb-2  ">
+                {period}  الخير👋
             </h1>
-            <h2 class="text-sm text-surface-500/50 ">
-                اليوم, الإثنين الموافق 25 يونيو
+            <h2 class="text-2xl text-surface-500/50 ">
+                {dayjs().format('اليوم هو, dddd D MMM YYYY')}
             </h2>
     
         </div>
-        <div class="bg-red-500 h-16 w-16 ">
+        <div class="bg-gray-300 h-16 w-32 ">
 
         </div>
     </header>
