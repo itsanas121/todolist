594a8514f3d7513ebc49fb2a6fe93e6cbd88f289
Anas <anascs2023@gmail.com>
2024-07-11 03:47:57 +0300

    hopefully it works

--- src/lib/TaskInput.svelte
+++ src/lib/TaskInput.svelte
@@ -22,9 +22,14 @@
 </script>
 
 <form
-  class="input-group input-group-divider flex flex-col sm:flex-row justify-between bg-white"
+  class="input-group input-group-divider flex flex-col sm:flex-row bg-white"
 >
-
+<!-- <textarea
+    bind:value={title}
+    class="flex-1 text-xl text-center"
+    placeholder="عنوان المهمة"
+    style="border: none; border-bottom-left-radius: 0;"
+></textarea> -->
   <input
     bind:value={title}
     class="flex-1 text-xl"
@@ -33,15 +38,15 @@
     style="word-wrap: break-word;"
   />
   <div class="container flex flex-col !sm:justify-end !items-end !text-center max-w-96">
-    <div class="!justify-items-center ml-10 text-[#0047AB]">تاريخ المهمة</div>
+    <div class="!justify-items-center ml-10 text-[#0047AB] "> تاريخ المهمة</div>
     <input
       bind:value={datetime}
-      class="input sm:w-fit"
+      class="input sm:w-fit !max-w-96"
       title="Input (datetime-local)"
       type="datetime-local"
     />
   </div>
-
+</form>
   <button
     type="submit"
     on:click={addTask}
@@ -51,4 +56,4 @@
   >
     <span class="mx-auto">إضافة</span>
   </button>
-</form>
+
